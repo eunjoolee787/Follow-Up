83a26cbee1199c655b7ab0e50ba5edfde7106c50
Eun Joo <eunjoolee787@gmail.com>
2015-03-07 17:17:25 -1000

    Add forms in form.jade file with first & last name fields

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+app.get('/form', function (req, res) {
+  res.render('form');
+});
+
 // app.get('/', function (req, res) {
 //   res.send('Hello World!');
 // });
