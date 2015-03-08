fb23d1800ef1097e1ed383ef75d0a2ae358f7f71
Eun Joo <eunjoolee787@gmail.com>
2015-03-07 20:00:53 -1000

    Add config.json

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require ('express');
-var app = express();
-
+var bodyParser = require('body-parser');
 var path = require ('path');
+var app = express();
+var config = require('../config');
 
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
   res.render('index');
@@ -14,8 +16,14 @@ app.get('/form', function (req, res) {
   res.render('form');
 });
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
+app.post('/form', function (req, res) {
+  // res.send('You sent the name "' + req.body.name + '".');
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  console.log(req.body);
+});
+
+module.exports.app = app;
+module.exports = config;
 
 app.listen(3000);
