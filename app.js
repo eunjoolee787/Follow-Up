f4b3c2187bb46af25832c0f75f73ce106adcd869
Eun Joo <eunjoolee787@gmail.com>
2015-03-11 13:01:09 -1000

    save new Prospect data

--- app.js
+++ app.js
@@ -35,13 +35,96 @@ app.post('/form', function (req, res) {
   
   //create a new prospect & save then send res.json/res.send with it
   //mongoose save to database
-  var locals = {
-    firstname: req.body.firstname + ' ' + req.body.lastname
-  };
-  res.render('./views/form.jade', locals);
-});
+
+//   var prospect = {
+//     firstname: req.params.firstname,
+//     lastname: req.params.lastname
+//   };
+
+//   // var data = new prospectSchema(prospect);
+
+//   Prospect.save(function (error, Prospect) {
+//     if(error) {
+//       console.log(error);
+//       res.json(Prospect);
+//     } else {
+//       res.json(Prospect);
+//     }
+//   });
+// });
+  var prospect = new Prospect({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname
+  });
+  prospect.save(function (err, prospect) {
+    if (err) { return next(err); }
+      res.json(201, prospect);
+    });
+  });
+
+  // var locals = {
+  //   firstname: req.body.firstname + ' ' + req.body.lastname
+  // };
+  // res.render('./views/form.jade', locals);
+
+//   var prospect_data = {
+//     firstname: req.params.first,
+//     lastname: req.params.last
+//   };
+//   var prospect = new Prospect (prospect_data);
+
+//   prospect.save(function(error, data) {
+//     if(error) {
+//       res.json(error);
+//     } else {
+//       res.json(data);
+//     }
+//   });
+// });
+
+//   Prospect.find({}, function (error, data) {
+//     console.log(data);
+//     res.json(data);
+//   });
+
+  // var account_info = {
+  //   firstname: req.body.firstname,
+  //   lastname: req.body.lastname
+  // };
+  // Prospect.save(function(err, user) {
+  //   if (err) {
+  //     throw err;
+  //   } else {
+  //     res.json(data);
+  //   }
+  // });
+
+  // req.body.info = req.prospect.firstname + ' ' + req.prospect.lastname;
+
+  // var new_info = new Prospect(req.body);
+  //   new_info.save(function (err, prospect) {
+  //     if(err) {
+  //       throw err;
+  //     } else {
+  //       res.json(data);
+  //     }
+  //   });
+
+//   var new_user = new User({
+//     firstname: req.body.firstname,
+//     lastname: req.body.lastname
+//   });
+
+//   new_user.save(function (err, prospect) {
+//     if(err) {
+//       throw err;
+//     } 
+//     res.json(data);
+//   });
+// });
 
 module.exports.app = app;
 // module.exports = config;
+
 
 app.listen(4000);
