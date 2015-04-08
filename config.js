540754fe394cdb83402cdf9247c29e9277a7e5f6
Eun Joo <eunjoolee787@gmail.com>
2015-04-07 18:21:51 -1000

    Change config.js file

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-module.exports = {
-  "port": process.env.PORT,
-  "secret": process.env.SECRET,
-  "mongo": process.env.MONGO,
-  "salt": process.env.SALT,
-  "email": {"user": process.env.EMAIL_USER, "pass": process.env.EMAIL_PASS},
+if (process.env.SECRET){
+  module.exports = {
+    "port": process.env.PORT,
+    "secret": process.env.SECRET,
+    "mongo": process.env.MONGO,
+    "salt": process.env.SALT,
+    "email": {"user": process.env.EMAIL_USER, "pass": process.env.EMAIL_PASS},
+  }
+} else {
+  module.exports = require("./config.json");
 }
