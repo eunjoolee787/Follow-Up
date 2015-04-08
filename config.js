727d4abbc37ae73f72bc497ba80a52266afa6974
Eun Joo <eunjoolee787@gmail.com>
2015-04-07 17:39:51 -1000

    Add config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "port": process.env.PORT,
+  "secret": process.env.SECRET,
+  "mongo": process.env.MONGO,
+  "salt": process.env.SALT,
+  "email": "user": process.env.EMAIL_USER, "pass": process.env.EMAIL_PASS},
+}
