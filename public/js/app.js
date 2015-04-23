cb6489c89c4421b07a58ace3b892086d6e4f572f
Eun Joo <eunjoolee787@gmail.com>
2015-04-22 19:56:12 -1000

    Work on scrolling issue

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ angular.module('starter', ['ionic',
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
-      // window.cordova.plugins.Keyboard.disableScroll(true);
+      cordova.plugins.Keyboard.disableScroll(true);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
