c359b6744217e8649e99cd5bb44bbad4d183f8a9
Eun Joo <eunjoolee787@gmail.com>
2015-04-24 19:25:54 -1000

    change keyboard to false

--- public/js/app.js
+++ public/js/app.js
@@ -16,8 +16,8 @@ angular.module('starter', ['ionic',
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
-      // window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      window.cordova.plugins.Keyboard.disableScroll(true);
+      window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+      // window.cordova.plugins.Keyboard.disableScroll(true);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
