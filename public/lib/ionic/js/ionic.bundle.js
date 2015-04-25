d339c1eae9ed30ba91e457bb99be40b6c7c9f8c1
Eun Joo <eunjoolee787@gmail.com>
2015-04-24 21:13:35 -1000

    comment out scrolltop only

--- public/lib/ionic/js/ionic.bundle.js
+++ public/lib/ionic/js/ionic.bundle.js
@@ -3617,8 +3617,8 @@ function keyboardBrowserFocusIn(e) {
 
   document.addEventListener('keydown', keyboardOnKeyDown, false);
 
-  document.body.scrollTop = 0;
-  // document.body.querySelector('.scroll-content').scrollTop = 0;
+  // document.body.scrollTop = 0;
+  document.body.querySelector('.scroll-content').scrollTop = 0;
 
   keyboardActiveElement = e.target;
 
