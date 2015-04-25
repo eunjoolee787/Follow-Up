f1b26facd568d56f0c002a861ea9a47d14f89b27
Eun Joo <eunjoolee787@gmail.com>
2015-04-24 20:53:46 -1000

    disableScroll

--- public/js/app.js
+++ public/js/app.js
@@ -15,10 +15,13 @@ angular.module('starter', ['ionic',
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      window.cordova.plugins.Keyboard.disableScroll(true);
-    }
+    // if (window.cordova && window.cordova.plugins.Keyboard) {
+    //   window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    //   window.cordova.plugins.Keyboard.disableScroll(true);
+    // }
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+          window.cordova.plugins.Keyboard.disableScroll(true);
+        }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
