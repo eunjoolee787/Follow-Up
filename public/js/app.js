d0fa74d5e74c309af762b4bb9121a8ed5a07db0e
Eun Joo <eunjoolee787@gmail.com>
2015-04-24 19:55:27 -1000

    Reset disable scroll to true

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ angular.module('starter', ['ionic',
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       // cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
-      cordova.plugins.Keyboard.disableScroll(false);
+      cordova.plugins.Keyboard.disableScroll(true);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
